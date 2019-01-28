49067340f5536097e1483d4d4e00618f3323bdb1
浅忆梦微凉 <3538922590@qq.com>
2019-01-28 22:02:23 +0800

    1

--- 剑指offer18_矩形覆盖/test.cpp
+++ 剑指offer18_矩形覆盖/test.cpp
@@ -1,31 +1,41 @@
 #include<iostream>
 using namespace std;
 
-class Solution {
-public:
-	int rectCover(int number) {
-		if (number <= 0) {
-			return number;
-		}
-		int f1 = 1;
-		int f2 = 2;
-		int f3;
+//class Solution {
+//public:
+//	int rectCover(int number) {
+//		if (number <= 0) {
+//			return number;
+//		}
+//		int f1 = 1;
+//		int f2 = 2;
+//		int f3;
+//
+//		for (int i = 3; i <= number; i++) {
+//			f3 = f1 + f2;
+//			f1 = f2;
+//			f2 = f3;
+//		}
+//		return f3;
+//	}
+//};
 
-		for (int i = 3; i <= number; i++) {
-			f3 = f1 + f2;
-			f1 = f2;
-			f2 = f3;
-		}
-		return f3;
+
+int rectCover(int number) {
+	if (number <= 0) {
+		return number;
 	}
-};
-
-void TestFunc(){
-	Solution s;
-	auto temp=s.rectCover(4);
-	cout << temp << endl;
-
+	return (rectCover(number - 1) + rectCover(number - 2));
 }
+
+
+
+//void TestFunc(){
+//	Solution s;
+//	auto temp=s.rectCover(4);
+//	cout << temp << endl;
+//
+//}
 
 int main(){
 	TestFunc();
